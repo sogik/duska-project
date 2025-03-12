24eda53b02fd5c445d46cbce7390a49920742470
sogik <itssogik@gmail.com>
2025-03-12 12:20:00 +0100

    Correcion de errores

--- server/basedatos.h
+++ server/basedatos.h
@@ -7,7 +7,7 @@ int usuarioExiste(MYSQL *conn, const char* nombre_usuario);
 int insertarUsuario(MYSQL *conn, const char* nombre_usuario, const char* contrasena);
 int verificarCredenciales(MYSQL *conn, const char* nombre_usuario, const char* contrasena);
 int desconectar_base_datos(MYSQL *conn);
-char* listarJugadores(MYSQL *conn);
+void listarJugadores(MYSQL *conn, char *lista, int tamano_lista);
 char* listarPartidas(MYSQL *conn);
 char* listarPartidasGanadas(MYSQL *conn, const char* nombre_usuario);
 
