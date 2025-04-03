3e10a63d7eb5c62ef96d43538e0a0d91e6035d03
sogik <itssogik@gmail.com>
2025-04-03 14:49:14 +0200

    cambiar el puerto de escucha del servidor de 9050 a 50756

--- server/server.c
+++ server/server.c
@@ -110,7 +110,7 @@ int main(int argc, char *argv[]) {
     memset(&serv_adr, 0, sizeof(serv_adr));
     serv_adr.sin_family = AF_INET;
     serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_adr.sin_port = htons(9050);
+    serv_adr.sin_port = htons(50756);
 
     if (bind(sock_listen, (struct sockaddr *) &serv_adr, sizeof(serv_adr)) < 0) {
         perror("Error al bind");
