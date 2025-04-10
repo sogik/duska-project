9f8adc249f18bb86feb3d9ec278ff2eeded04d8d
sogik <itssogik@gmail.com>
2025-04-10 17:16:02 +0200

    subida de cliente

---
+++ client/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GeonBit.UI.Examples
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new GeonBitUI_Examples())
+            {
+                game.Run();
+            }
+        }
+    }
+}
